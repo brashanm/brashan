b32fec304808d3867f89234d2585e58157e623f0
Brashan <brashanm@gmail.com>
2024-07-02 18:08:06 -0400

    background

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ const GlobalStyle = createGlobalStyle`
   html, body {
     width: 100%;
     height: 100%;
-    background-color: #111;
-    // background-color: black;
+    background-color: black;
   }
 `;
 
