43e68b4d42d8ff3d3d6f362b98a513ed5575e85d
Brashan <brashanm@gmail.com>
2025-05-12 14:14:20 -0400

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -105,8 +105,8 @@ function Home() {
             <EasterEggs />
             <Body>
                 <Block>
-                    <b>Welcome to my (small) slice of the internet!</b> I'm a 4A Computer Science student at the <Linked href="https://uwaterloo.ca/" target="_blank">University of Waterloo</Linked>.
-                    This summer, I'll be doing some really cool stuff as a software engineer intern at <Linked href={"https://www.linkedin.com/company/stackadapt/"} target="_blank">StackAdapt</Linked>, on the Customer Data Platform team.
+                    <b>Welcome to my (small) slice of the internet!</b> I'm a 4B Computer Science student at the <Linked href="https://uwaterloo.ca/" target="_blank">University of Waterloo</Linked>.
+                    This summer, I'll be doing some really cool stuff as a software engineering intern at <Linked href={"https://www.linkedin.com/company/stackadapt/"} target="_blank">StackAdapt</Linked>, on the Customer Data Platform team.
                 </Block>
                 <Block>
                     <List description="Previously, I..." items={internships} />
@@ -115,6 +115,7 @@ function Home() {
                     <List description="I'm not always coding though! Outside of tech, you can find me..." items={hobbies} />
                 </Block>
                 <Block>Be it making it easy for gym enthusiasts to track their progress with <Linked href="https://github.com/brashanm/MtProgress" target="_blank">Mount Progress</Linked>, spreading The Weeknd's lyrics worldwide through multilingual translations with <Linked href="https://github.com/brashanm/Abelese" target="_blank">Abelese</Linked>, or my current venture, <b>Anther</b>, which aims to revolutionize health and wellness through AI, I've always been driven to innovate and create impactful projects that touch the lives of many.</Block>
+                <Block>I'm currently seeking <b>2026 new grad</b> opportunities!</Block>
                 <Block>Whether it's about new opportunities, someone to play poker with, or sitcom recommendations, I'm always eager to connect with more people! Feel free to reach out <Linked href="https://www.linkedin.com/in/brashan/" target="_blank">here</Linked> or through my <Linked href="mailto:bmohanak@uwaterloo.ca" target="_blank">email</Linked>.</Block>
             </Body>
             <Footer><h4>Made with <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Activities/Heart%20Suit.png" alt="Heart Suit" width="25" height="25" /> by Brashan Mohanakumar</h4></Footer>
